2e0257501a12958cf8c4cd4fdbb895a41f5840df
Wenzhen Yuan <wenzhen.yuan@tri.global>
2018-09-24 17:27:05 -0400

    adding the new in-hand manipulation demo

---
+++ examples/allegro_hand/in_hand_manipulation/mug_state_set.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "drake/common/eigen_types.h"
+#include "drake/math/rotation_matrix.h"
+#include "drake/lcm/drake_lcm.h"
+
+namespace drake {
+namespace examples {
+namespace allegro_hand {
+
+class SetMugStateControl{
+  public:
+    SetMugStateControl();
+
+    // return the 4 frames of the preset positions of the target of the
+    // fingertips when only grasping on the mug based on the mug pose.
+    void GetGraspTargetFrames(const Isometry3<double>& obj_frame, 
+                        std::vector<Isometry3<double>>* finger_frames,
+                        std::vector<Isometry3<double>>* relative_finger_pose);
+
+    void GetXRotatedTargetFrame(const double rotation_angle,
+                              std::vector<Isometry3<double>>* finger_frames);
+    void GetYRotatedTargetFrame(const double rotation_angle,
+                              std::vector<Isometry3<double>>* finger_frames);
+    void GetZRotatedTargetFrame(const double rotation_angle,
+                              std::vector<Isometry3<double>>* finger_frames);
+    void GetTransTargetFrame(const Vector3<double> translation_vector,
+                              std::vector<Isometry3<double>>* finger_frames);
+
+    void UpdateMugPose(const Isometry3<double>& mug_frame) {
+        ref_mug_pose_ = mug_frame;
+    }
+
+    void PublishTargetFrametoLcm(const Isometry3<double>& mug_frame);
+
+  private:
+
+    // frames of the selected fingertip frame on the cup
+    std::vector<Isometry3<double>> contact_mug_frames_;
+
+    // the saved reference of mug position
+    Isometry3<double> ref_mug_pose_;
+
+    // dimension of the mug
+    const double MugHeight = 0.14;
+    const double MugRadius = 0.04;
+    // parameters about the grasp point
+    const double central_point = MugHeight / 2;
+    const double index_finger_interval = 0.045;
+    const double thumb_partial = 0.007;
+
+};
+
+void PublishFramesToLcm(const std::string& channel_name,
+                        const std::vector<Eigen::Isometry3d>& poses,
+                        const std::vector<std::string>& names,
+                        drake::lcm::DrakeLcmInterface* dlcm);
+
+}  // namespace allegro_hand
+}  // namespace examples
+}  // namespace drake
---
+++ examples/allegro_hand/in_hand_manipulation/object_pose_publisher.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "drake/multibody/multibody_tree/multibody_plant/multibody_plant.h"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/lcm/drake_lcm.h"  
+
+#include "robotlocomotion/pose_t.hpp"
+
+namespace drake {
+namespace examples {
+namespace allegro_hand {
+
+using drake::multibody::multibody_plant::MultibodyPlant;
+
+const double kObjectStatePublishPeriod = 0.05;
+
+/// The class track the pose of the target object in the plant, and publish it
+/// to LCM channel
+class ObjectPosePublisher : public systems::LeafSystem<double> {
+public:
+  ObjectPosePublisher(const MultibodyPlant<double>& plant, 
+                     const std::string& obj_body_name);
+
+  const systems::InputPort<double>& get_state_input_port() const {
+    return this->get_input_port(state_input_port_);
+  }
+
+private:
+  void DoPublish(const systems::Context<double>& context,
+                 const std::vector<const systems::PublishEvent<double>*>& events)
+                 const override;
+
+  const MultibodyPlant<double>* plant_{nullptr};
+  const std::string obj_body_name_;
+  std::unique_ptr<systems::Context<double>> plant_context_;
+  int state_input_port_{-1};
+
+  // ::lcm::LCM lcm_;
+  const std::string kObjectPoseLCMChannel = "TARGET_OBJ_POSE";
+};
+
+
+
+}  // namespace allegro_hand
+}  // namespace examples
+}  // namespace drake
+
